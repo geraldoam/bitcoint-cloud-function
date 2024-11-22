20c759d9cce55fe9a791e7fe630e2e8d03055cd0
Geraldo Ahnert <geraldoahnert@gmail.com>
2024-11-22 12:48:20 -0300

    get informations from coigecko public api

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import functions_framework
 
-import request
+import requests
 
 @functions_framework.http
 def bitcoin(request):
-  
+
+  url = "https://api.coingecko.com/api/v3/simple/price"
+
+  params = {
+    "ids": "bitcoin,usd",
+    "vs_currencies": "usd,brl"
+  }
+
+  response = requests.get(url, params=params)
+  data = response.json()
+
+  btc_usd = data["bitcoin"]["usd"]
+  btc_brl = data["bitcoin"]["brl"]
+  usd_brl = data["usd"]["brl"]
+
+  return data
